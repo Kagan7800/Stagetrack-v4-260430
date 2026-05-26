09ead7b79fbfddd0886ef3eac751080681dc5a21
Kagan7800 <your-email@example.com>
2026-05-26 11:53:34 -0400

    Increment Lobby.svg version parameter to v=3 to bypass browser caching

--- src/components/LobbyOverlay.jsx
+++ src/components/LobbyOverlay.jsx
@@ -106,7 +106,7 @@ export default function LobbyOverlay() {
         ref={containerRef}
         style={{ '--lobby-scale': scale }}
       >
-        <img src="/assets/Lobby.svg?v=2" className="lobby-svg-img" alt="Lobby Background" />
+        <img src="/assets/Lobby.svg?v=3" className="lobby-svg-img" alt="Lobby Background" />
         <img src="/assets/lobby_rect.png" className="lobby-card-panel-1" alt="" />
         <img src="/assets/lobby_rect.png" className="lobby-card-panel-2" alt="" />
 
